7c67caca8ff2db6c23a4d2f29c602cffb2487fc2
nyuwol <nyuwol@gamil.com>
2017-07-27 14:17:17 +0900

    a

---
+++ app/src/main/java/com/example/pc/mm/Map.java
@@ -0,0 +1,15 @@
+package com.example.pc.mm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pc.locationapp.R;
+
+public class Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
