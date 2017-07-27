e847676e2064400abdebe684c68b6d1cbd4ff2e2
nyuwol <nyuwol@gamil.com>
2017-07-27 14:59:13 +0900

    a

---
+++ app/src/main/java/com/example/pc/mm/MapSearch.java
@@ -0,0 +1,26 @@
+package com.example.pc.mm;
+
+import android.app.Dialog;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.MapView;
+
+import java.util.List;
+
+public class MapSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_search);
+
+        
+    }
+}
